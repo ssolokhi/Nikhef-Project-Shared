aa80b531bad18fed579e22357395b32334c87b40
yagmurzb <167066936+yagmurzb@users.noreply.github.com>
2024-06-06 12:33:12 +0200

    Update dac_altai.py
    dac scan for VCASN range 55-59 for ALTAI

--- alpide-daq-software/scans/dac_altai.py
+++ alpide-daq-software/scans/dac_altai.py
@@ -7,7 +7,7 @@ from time import sleep
 from tqdm import tqdm
 import os
 
-DACS = [
+DACS=[
     'VRESETP',
     'VRESETD',
     'VCASP',
@@ -24,7 +24,7 @@ DACS = [
     'ITHR',
 ]
 
-VDACSel = {
+VDACSel={
     'VCASN':0,
     'VCASP':1,
     'VPULSEH':2,
@@ -35,9 +35,9 @@ VDACSel = {
     'VCLIP':7,
     'VTEMP':8,
     'ADCDAC':9
-    }
+}
 
-CDACSel = {
+CDACSel={
     'IRESET':0,
     'IAUX2':1,
     'IBIAS':2,
@@ -45,68 +45,58 @@ CDACSel = {
     'IREF':4,
     'ITHR':5,
     'IREFBuffer':6
-    }
+}
 
-
-
-def measure_dac_step(daq, idac, step,chipid,mode):
-        data=[]
-        dac = DACS[idac]
-        dac_addr = 0x601+idac
-        adc_input = 5 if dac_addr<0x60A else 6 # DACMONV or DACMONI
-        daq.alpide_reg_write(dac_addr, step, chipid)
-        daq.alpide_setreg_analog_monitor_and_override(
-            VoltageDACSel=VDACSel[dac] if dac in VDACSel.keys() else 0,
-            CurrentDACSel=CDACSel[dac] if dac in CDACSel.keys() else 0,
-            SWCNTL_DACMONI=0,SWCNTL_DACMONV=0,IRefBufferCurrent=1,chipid=chipid)
-
-        if mode:
-
-            for k,sel_input in [['Value',adc_input], ['AVDD',2]]: # measure AVDD and DAC
-                daq.alpide_setreg_adc_ctrl(
-                                          Mode=0,        #Manual
-                                          SelInput=sel_input,
-                                          SetIComp=2,
-                                          RampSpd=1,
-                                          DiscriSign=0,
-                                          HalfLSBTrim=0,
-                                          CompOut=0,chipid=chipid) 
-                daq.alpide_reg_write(0x0000, 0xFF20, chipid)
-                sleep(0.005) # wait >=5ms, according to ALPIDE manual           
-                output=daq.alpide_reg_read(0x613,chipid)
-                data.append(output)
+def measure_dac_step(daq, idac, step, chipid, mode):
+    data=[]
+    dac=DACS[idac]
+    dac_addr=0x601+idac
+    adc_input=5 if dac_addr<0x60A else 6
+    daq.alpide_reg_write(dac_addr, step, chipid)
+    daq.alpide_setreg_analog_monitor_and_override(
+        VoltageDACSel=VDACSel[dac] if dac in VDACSel.keys() else 0,
+        CurrentDACSel=CDACSel[dac] if dac in CDACSel.keys() else 0,
+        SWCNTL_DACMONI=0, SWCNTL_DACMONV=0, IRefBufferCurrent=1, chipid=chipid)
+    if mode:
+        for k, sel_input in [['Value', adc_input], ['AVDD', 2]]:
+            daq.alpide_setreg_adc_ctrl(
+                Mode=0,
+                SelInput=sel_input,
+                SetIComp=2,
+                RampSpd=1,
+                DiscriSign=0,
+                HalfLSBTrim=0,
+                CompOut=0, chipid=chipid)
+            daq.alpide_reg_write(0x0000, 0xFF20, chipid)
+            sleep(0.005)
+            output=daq.alpide_reg_read(0x613, chipid)
+            data.append(output)
+    else:
+        sleep(0.01)
+        if dac_addr<0x60A:
+            data=daq.read_dacmonv()
         else:
-
-             sleep(0.01) # wait >=5ms, according to ALPIDE manual 
-             if dac_addr<0x60A:
-                 data= daq.read_dacmonv()
-             else:
-                data= daq.read_dacmoni()
-        return (data) 
-
-
-
- 
+            data=daq.read_dacmoni()
+    return data
 
 now=datetime.datetime.now()
 parser=argparse.ArgumentParser(description='The dac scan test')
-parser.add_argument('--serial' ,'-s',help='serial number of the DAQ board')
-parser.add_argument('--chipid' ,'-c',type=int,help='Chip ID (default: 0x10)',default=0x10)
-parser.add_argument('--path' ,help='Path to directory for data saving',default=".")
-parser.add_argument('--via-on-chip-adc',help='Monitor DAC with ALPIDE ADCs',action='store_true')
+parser.add_argument('--serial', '-s', help='serial number of the DAQ board')
+parser.add_argument('--chipid', '-c', type=int, help='Chip ID (default: 0x10)', default=0x10)
+parser.add_argument('--path', help='Path to directory for data saving', default=".")
+parser.add_argument('--via-on-chip-adc', help='Monitor DAC with ALPIDE ADCs', action='store_true')
 args=parser.parse_args()
 
-
 if args.serial:
-    fname='dacscan-%s-%s.txt'%(args.serial,now.strftime('%Y%m%d_%H%M%S'))
+    fname='dacscan-%s-%s.txt' % (args.serial, now.strftime('%Y%m%d_%H%M%S'))
 else:
-    fname='dacscan-%s.txt'%(now.strftime('%Y%m%d_%H%M%S'))
+    fname='dacscan-%s.txt' % (now.strftime('%Y%m%d_%H%M%S'))
 
-if args.path != '.':  #creating new directory
+if args.path != '.':
     try:
         os.mkdir(args.path)
     except OSError:
-        print ("Creation of the directory %s failed" %args.path)
+        print("Creation of the directory %s failed" % args.path)
 
 try:
     daq=alpidedaqboard.ALPIDEDAQBoard(args.serial)
@@ -114,36 +104,31 @@ except ValueError as e:
     print(e)
     raise SystemExit(-1)
 
-
 daq.power_on()
-sleep(0.1) # let it settle
-iaa,idd,status=daq.power_status()
-print('IAA = %5.2f mA'%iaa)
-print('IDD = %5.2f mA'%idd)
+sleep(0.1)
+iaa, idd, status=daq.power_status()
+print('IAA = %5.2f mA' % iaa)
+print('IDD = %5.2f mA' % idd)
 if status:
     print('LDOs: ON')
 else:
     print('LDOs: OFF... too bad!')
     raise SystemExit(1)
 
-#--------------------------- init
-
-#just in case we got up on the wrong side #of the fw...
 daq.fw_reset()
-daq.alpide_cmd_issue(0xD2) # GRST for ALPIDE
-# now for monitoring, also start clean
+daq.alpide_cmd_issue(0xD2)
 daq.fw_clear_monitoring()
-daq.alpide_cmd_issue(0xE4) # PRST
+daq.alpide_cmd_issue(0xE4)
 
 if args.via_on_chip_adc:
-    daq.alpide_setreg_adc_ctrl(Mode=2,      #Automatic
-                              SelInput=0,  #AVSS
-                              SetIComp=2,  # 
-                              DiscriSign=0,
-                              RampSpd=1,   # 1mks
-                              HalfLSBTrim=0,
-                              CompOut=0,
-                              chipid=args.chipid)
+    daq.alpide_setreg_adc_ctrl(Mode=2,
+                               SelInput=0,
+                               SetIComp=2,
+                               DiscriSign=0,
+                               RampSpd=1,
+                               HalfLSBTrim=0,
+                               CompOut=0,
+                               chipid=args.chipid)
 
 daq.alpide_setreg_mode_ctrl(ChipModeSelector=0x0,
                             EnClustering=0x1,
@@ -161,23 +146,28 @@ daq.alpide_setreg_cmu_and_dmu_cfg(PreviousChipID=0x0,
                                   EnableDDR=0x1,
                                   chipid=args.chipid)
 
-#----------------------------- run
-
 daq.alpide_configure_dacs(chipid=args.chipid)
 sleep(1)
 print('Starting DAC scan...')
 
-of =open('%s/%s'%(args.path,fname),'w')
+of=open('%s/%s' % (args.path, fname), 'w')
 try:
-    for idac in tqdm(range(len(DACS)),desc="Overall progress"):
-        for step in tqdm(range(256),leave=False,desc="scan: %s"%(DACS[idac])):            
-            data=measure_dac_step(daq, idac, step,chipid=args.chipid,mode=args.via_on_chip_adc)
-            if args.via_on_chip_adc: 
-                of.write('%s\t%d\t%d\n'%(DACS[idac],step,data[0]))
-            else:
-                of.write('%s\t%d\t%d\n'%(DACS[idac],step,data)) 
+    for idac in tqdm(range(len(DACS)), desc="Overall progress"):
+        if DACS[idac] == 'VCASN':
+            for step in tqdm(range(55, 60), leave=False, desc="scan: %s" % (DACS[idac])):
+                data=measure_dac_step(daq, idac, step, chipid=args.chipid, mode=args.via_on_chip_adc)
+                if args.via_on_chip_adc:
+                    of.write('%s\t%d\t%d\n' % (DACS[idac], step, data[0]))
+                else:
+                    of.write('%s\t%d\t%d\n' % (DACS[idac], step, data))
+        else:
+            for step in tqdm(range(256), leave=False, desc="scan: %s" % (DACS[idac])):
+                data=measure_dac_step(daq, idac, step, chipid=args.chipid, mode=args.via_on_chip_adc)
+                if args.via_on_chip_adc:
+                    of.write('%s\t%d\t%d\n' % (DACS[idac], step, data[0]))
+                else:
+                    of.write('%s\t%d\t%d\n' % (DACS[idac], step, data))
 except KeyboardInterrupt:
     print('Test interrupted!')
 
 of.close()
-#daq.power_off()
