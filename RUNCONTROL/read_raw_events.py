a5a0fdf56881ccf7bc8f3b1b1eea00632fd29efb
quinten <quinten>
2024-06-10 13:53:57 +0200

    Added python run files, analysis script, and outputs from eudaq runs

---
+++ RUNCONTROL/read_raw_events.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+
+import pyeudaq as eu
+from alpidedaqboard.decoder import decode_event
+
+
+def getPixelCounts(reader):
+    hm = np.zeros((512,1024))
+
+    i = 0
+    ev = reader.GetNextEvent()
+    while ev:
+        #print("Event number:", i)
+
+        for j, sev in enumerate(ev.GetSubEvents()):
+            #print("\tsub event", j)
+            block = sev.GetBlock(0)
+            hits, iev, tev, k = decode_event(block, 0)
+            #print("\t", tev, hits)
+
+            for x,y in hits:
+                hm[y,x] += 1
+                #hm [y,x] = tev
+
+        ev = reader.GetNextEvent()
+        i += 1
+
+    return hm
+
+
+def truncatedHist(hm, ax, truncate=100, **kwargs):
+    # Log histogram of non zero hits, truncated
+    hits = hm[(hm != 0)] 
+    flattened = hm.flatten()
+    counts = Counter(hits)
+    print("max counts should be each pixel =", hm.size)
+    print(counts)
+
+    return ax.hist(hits[hits < truncate], bins = 100, **kwargs)
+
+
+fig, ax = plt.subplots()
+
+f = eu.FileReader("native", "tests/1min-loop-run241124058_240610124104(noblanket).raw")
+hm = getPixelCounts(f)
+truncatedHist(hm, ax, label="no blanket", alpha=0.6)
+
+f_blanket = eu.FileReader("native", "tests/1min-loop-run241130131_240610130137(blanket).raw")
+hm_blanket = getPixelCounts(f_blanket)
+truncatedHist(hm_blanket, ax, label='blanket', alpha=0.6)
+plt.legend()
+plt.show()
+
+# Scatter plot of non zero
+#nonzero_xy = np.nonzero(hm)
+#counts_xy = truncated[nonzero_xy]
+#print(nonzero_xy)
+#print(counts_xy)
+#plt.scatter(nonzero_xy[0], nonzero_xy[1], s=1)
+#plt.scatter(nonzero_xy[0], nonzero_xy[1], s=counts_xy, alpha=0.6)
+#plt.show()
+
+# Log scale hitmap
+#plt.imshow(np.log(hm+0.1))
+
+# Show non zero
+#plt.imshow((hm > 0) + (hm > 1), cmap='jet', vmin=0, vmax=2)
+
+# Masked array?
+#masked_array = np.ma.masked_where(hm == 0, hm)
+
+# Simpe imshow
+#plt.imshow(hm, cmap='jet', vmax=300)
+#plt.colorbar()
+#
+#plt.show()
