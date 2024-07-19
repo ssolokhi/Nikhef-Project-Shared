f3527ca3cb202161c3155c3f83abce9a61a7ecde
Sergei <94015310+ssolokhi@users.noreply.github.com>
2024-07-19 15:04:14 +0200

    Delete firmware

--- alpide-daq-fpga-firmware/report_timing.tcl
+++
@@ -1,9 +0,0 @@
-project_open alpide-daq-fpga-fw
-
-create_timing_netlist
-read_sdc constraints/timing.sdc
-update_timing_netlist
-
-report_timing -npaths 10
-
-project_close
--- alpide-daq-fpga-firmware/scripts/jicnsvf.tcl
+++
@@ -1,2 +0,0 @@
-qexec {quartus_cpf -c -s EP4CE40F23C6 -d EPCS16 alpide-daq-fpga-fw.sof foo.jic}
-qexec {quartus_cpf -c foo.jic -n p -g 3.3 -q 10MHz foo.svf}
